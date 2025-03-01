62e4cc6dea51472e91fdee958036d5677ee073c3
jchagas <joao.juchum36@gmail.com>
2025-03-01 21:15:22 +0100

    Exercise 1 done

--- JavaScript_basics/exercise1.mjs
+++ JavaScript_basics/exercise1.mjs
@@ -15,10 +15,41 @@ export function fibonaIt(n) {
 }
 
 // Recursive programming
-export function fiboRec(n) {}
+export function fiboRec(n) {
+    if (n <= 1) return n;
+    return fiboRec(n - 1) + fiboRec(n - 2);
+}
 
 // Process array, no map
-export function fiboArr() {}
+export function fiboArr(t) {
+    let arr = [];
+    for (let i = 0; i < t.length; i++) {
+        let a, b, c;
+        a = 0;
+        b = 1;
+        for (let j = 2; j <= t[i]; j++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        if (t[i] === 0) arr.push(a);
+        else arr.push(b);
+    }
+    return arr;
+}
 
 // No loop
-export function fibonaMap() {}
+export function fibonaMap(t) {
+    return t.map(n => {
+        let a, b, c;
+        a = 0;
+        b = 1;
+        for (let i = 2; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        if (n === 0) return a;
+        else return b;
+    });
+}
--- JavaScript_basics/test_exercise1.mjs
+++ JavaScript_basics/test_exercise1.mjs
@@ -8,5 +8,18 @@ console.log(fibonaIt(1));
 console.log(fibonaIt(5));
 console.log(fibonaIt(42));
 
-//console.log(fiboRec(8));
-//console.log(fiboArr([3,5]));
+// Tests for fiboRec
+console.log(fiboRec(8));
+console.log(fiboRec(13));
+console.log(fiboRec(21));
+console.log(fiboRec(34));
+
+// Tests for fiboArr
+console.log(fiboArr([3,5]));
+console.log(fiboArr([8,13]));
+console.log(fiboArr([21,34]));
+
+// Tests for fibonaMap
+console.log(fibonaMap([3,5]));
+console.log(fibonaMap([8,13]));
+console.log(fibonaMap([21,34]));
